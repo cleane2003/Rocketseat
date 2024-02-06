005ccb78c038314fb83748fe7d8c2eeec042421b
Cleane Evelin <cleaneevelin04@gmail.com>
2024-02-05 22:57:43 -0300

    create habits

--- nwl-2023/backend/src/server.ts
+++ nwl-2023/backend/src/server.ts
@@ -1,16 +1,12 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "./lib/prisma";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
-
-app.get('/', () => {
-    const habits = prisma.habit.findMany();
-    return habits;
-}) 
+app.register(appRoutes);
 
 app.listen({
     port: 3333,
